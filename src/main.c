3a2631116a619485c2d2e22e1cdcd5546ee0377d
a-grigorov <145719808+a-grigorov@users.noreply.github.com>
2025-05-08 20:46:50 +0300

    - orthographical projection for points

--- src/main.c
+++ src/main.c
@@ -4,8 +4,32 @@
 #include <stdint.h>
 #include <stdlib.h>
 #include <SDL3/SDL.h>
+#include "vector.h"
+
+const int N_POINTS = 9 * 9 * 9;
 
 bool is_running = false;
+float fov_scale = 250.0f;
+vec3_t cube_points[N_POINTS];
+vec2_t projected_points[N_POINTS];
+
+void setup_cube_points(void) {
+	float step = 2 / (float)9;
+	size_t index = 0;
+
+	for (size_t x = 0; x < 9; x++) {
+		for (size_t y = 0; y < 9; y++) {
+			for (size_t z = 0; z < 9; z++) {
+				vec3_t point;
+				point.x = (x * step) - 1;
+				point.y = (y * step) - 1;
+				point.z = (z * step) - 1;
+				cube_points[index] = point;
+				index++;
+			}
+		}
+	}
+}
 
 void setup(void) {
 	// Allocate memory for collor buffer
@@ -19,6 +43,9 @@ void setup(void) {
 		window_width,
 		window_height
 	);
+
+	// Fill the cube_points array with points in the range [-1, 1]
+	setup_cube_points();
 }
 
 void process_input(void) {
@@ -37,33 +64,33 @@ void process_input(void) {
 	}
 }
 
-void update(void) {
+vec2_t project(vec3_t point) {
+	vec2_t projected_point = { point.x, point.y };
+	return projected_point;
+}
 
+void update(void) {
+	for (size_t i = 0; i < N_POINTS; i++)
+	{
+		vec3_t point = cube_points[i];
+		vec2_t projected_point = project(point);
+		projected_point.x = (projected_point.x * fov_scale) + (window_width / 2);
+		projected_point.y = (projected_point.y * fov_scale) + (window_height / 2);
+		projected_points[i] = projected_point;
+	}
 }
 
 void render(void) {
-	SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
-	SDL_RenderClear(renderer);
-
 	draw_grid(0x0000FFFF);
-	draw_rect(100, 100, 200, 100, 0x00FF00FF);
 
-	const int rect_pos_x = 80;
-	const int rect_pos_y = 80;
-
-	const int rect_width = 40;
-	const int rect_height = 40;
-
-	for (int i = rect_pos_x; i < rect_pos_x + rect_width; i++)
+	for (size_t i = 0; i < N_POINTS; i++)
 	{
-		for (int j = rect_pos_y; j < rect_pos_y + rect_height; j++)
-		{
-			draw_pixel(i, j, 0xFF00FFFF);
-		}
+		vec2_t point = projected_points[i];
+		draw_rect(point.x, point.y, 4, 4, 0xFFFF00FF);
 	}
 
 	render_color_buffer();
-	clear_color_buffer(0xFFFF00FF);
+	clear_color_buffer(0x000000FF);
 
 	SDL_RenderPresent(renderer);
 }
